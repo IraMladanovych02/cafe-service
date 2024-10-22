2e15d60fd0ffca5ef3893affc063c823f44d2d31
Ira <iramladanovych@gmail.com>
2024-10-22 22:15:02 +0300

    Implement index view

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from cafe.models import Dish, DishType, Cook
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    context = {
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_cooks": num_cooks
+    }
+    return render(request, "cafe/index.html", context=context)
